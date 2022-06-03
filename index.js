cb9fb2edcc8b0d2917ab0c9377d5f714f319c386
Edwin Prastyan <edwinprastyan88@gmail.com>
2022-06-03 21:41:11 +0700

    update index.js

--- index.js
+++ index.js
@@ -6,15 +6,30 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (namesArray, sortProcess) =>{
+  
+  const result = sortProcess(namesArray);
+  let resultArray = [];
+  let count = 1;
+  for(let i=0; i<result.length;i++){
+    let concat = count +'. '+result[i];
+    resultArray.push(concat);
+    count++;
+  }
+ return resultArray;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (namesArray) => {
+  return namesArray.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (namesArray) => {
+  return namesArray.reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
